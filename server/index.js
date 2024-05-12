d9489bff7811320b09f730992472392c7d6e0cf2
Agni Noor <agninoor04@gmail.com>
2024-05-12 19:17:18 +0600

    server setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const CONNECTION_URL = "mongodb://localhost:27017/MemoryShareDB";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
