bbce3dd9958a9c5abd5d06afb07744b6f28197de
Agni Noor <agninoor04@gmail.com>
2024-05-13 20:02:34 +0600

    app.js updated in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 
+import momentshare from "./images/momentshare.png";
+
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          MomentShare
+        </Typography>
+        <img
+          src={momentshare}
+          alt="momentshare"
+          height="500"
+          width="500"
+          align="center"
+        />
+      </AppBar>
+    </Container>
   );
 };
 
